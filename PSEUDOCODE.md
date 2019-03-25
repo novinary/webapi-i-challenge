1c32ac4a32780601a64e5229fe7d1b8d0603f854
novinary <novina.dev@gmail.com>
2019-03-25 20:37:20 +0000

    Work on the GET method endpoints

--- PSEUDOCODE.md
+++ PSEUDOCODE.md
@@ -7,5 +7,10 @@ Step 1
 Step 2
 - Get the server to listen - Done 
 
-Step 3
-- Handle get request to our roots. Get takes endpoints and a callback 
+Step 3 ** GET METHOD ENDPOINTS**
+Handle get request to our roots. Get takes endpoints and a callback 
+- create endpoint to get all users contained in db - done
+    - make a call to the db - done
+    - two cases - either we are finding a user or not
+    in both cases we are sending a status and response every time 
+- create endpoint to get a user by id (same as above but find by id)- done
--- index.js
+++ index.js
@@ -2,22 +2,40 @@
 
 const express = require("express");
 
+const db = require('./data/db');
+
 const server = express();
 const PORT = 4000;
 
 // endpoints
 
-server.get("/greet", (req, res) => {
-  res.send("Hello");
+server.get("/api/users", (req, res) => {
+    db.find()
+        .then((users) => {
+            res.json(users);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ message: "failed to get users" });
+        });
 });
 
-server.get("/greet/:name", (req, res) => {
-  const name = req.params.name;
-  res.send(`Hello ${name}`);
+server.get("/api/users/:id", (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+        .then(user => {
+            res.json(user);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ message: "failed to get user" });
+        })
 });
 
 // listening
 
 server.listen(PORT, () => {
-  console.log(`server is up and running on port ${PORT}`);
+    console.log(`server is up and running on port ${PORT}`);
 });
